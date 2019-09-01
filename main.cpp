98f0006566a6fc8e4bc3be2f7dd62369d49d0b32
rgbk21 <rajgaurav.bk@gmail.com>
2019-09-01 10:55:12 -0500

    - Updated ReadMe

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ bool useMaxSeed = true;//Applicable if someCondition = false. Set to false if us
 bool someCondition = true;//Set to true if calculating maxSeed BEFORE removing any nodes. Remove nodes ONLY if they arent in maxSeed.
 bool useRandomSeed = false;//Set to true (someCondition should also be set to true) if using random Seed instead of max Seed as the initial seed BEFORE finding the vertices to be removed
 bool useEnvelop = false;//Set to true if someCondition is set to true. Implies you are not removing nodes from the envelopedNodes as well. SeedSet is still fixed.
-bool useSeedProvidedAsInput = true;//If this is set to TRUE, instead of calulating the maxSeed and using that as the seed, we use the nodes provided by the user in a text file as the seed. You will have to manually set the file name in the code
+bool useSeedProvidedAsInput = false;//If this is set to TRUE, instead of calulating the maxSeed and using that as the seed, we use the nodes provided by the user in a text file as the seed. You will have to manually set the file name in the code
 //These are my global variables for testing
 vector<int> modNodesToRemoveUnsorted;
 vector<int> subModTopKInflNodesToRemoveUnsorted;
@@ -1452,8 +1452,7 @@ void executeTIMTIMfullGraph(cxxopts::ParseResult result) {
                 //This code will run when someCondition is true AND envelopedNodes is true.
                 //THis implies you cannot remove anything from the seed Set OR the envelopNodes as well
                 envelopedNodes = getSeed((budget + 100), maxSeedGraph, activatedSet, set<int>(), set<int>(), set<int>(),
-                                         set<int>(),
-                                         NULL);    //Find the top (budget + 100) nodes that will form a part of the envelop
+                                         set<int>(), NULL);    //Find the top (budget + 100) nodes that will form a part of the envelop
             }
             if (useSeedProvidedAsInput){
                 string fileName = "Mapped_Seed_To_v2.txt";
